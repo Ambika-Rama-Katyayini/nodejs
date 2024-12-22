c4d84c1713042ec5783accbda8ae5b0d27e8a955
Ambika-Rama-Katyayini <ambikaramakatyayini.k@gmail.com>
2024-12-22 20:42:11 +0530

    nodemailer

--- day8/index.js
+++ day8/index.js
@@ -1,1 +1,116 @@
 var nm = require('nodemailer');
+var transporter = nm.createTransport(
+    {
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth:{
+            user: "demo47942@gmail.com",
+            pass: "pass-key",
+        }
+    }
+);
+var options = {
+    from: "demo47942@gmail.com",
+    to: "kkatyaini1@gmail.com",
+    subject: "test mail from nodemailer module",
+    text: "Nodeemail sending 'nodemailer' test message"
+}
+transporter.sendMail(
+    options, function(error_info, success_info){
+        if(error){
+            console.log(error_info);
+        }
+        else{
+            console.log("Send Successfully");
+        }
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
